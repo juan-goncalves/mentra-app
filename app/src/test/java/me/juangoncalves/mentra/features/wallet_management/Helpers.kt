51735b79e3611613745497d2c53640ab5910e693
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-16 21:30:54 -0400

    Create file to store helpers for each module's tests

---
+++ app/src/test/java/me/juangoncalves/mentra/features/wallet_management/Helpers.kt
@@ -0,0 +1,7 @@
+package me.juangoncalves.mentra.features.wallet_management
+
+import me.juangoncalves.mentra.features.wallet_management.domain.entities.Coin
+
+val Bitcoin = Coin(name = "Bitcoin", symbol = "BTC", imageUrl = "http://url.com/btc.jpg")
+val Ethereum = Coin(name = "Ethereum", symbol = "ETH", imageUrl = "http://url.com/eth.jpg")
+val Ripple = Coin(name = "Ripple", symbol = "XRP", imageUrl = "http://url.com/xrp.jpg")
--- app/src/test/java/me/juangoncalves/mentra/features/wallet_management/domain/usecases/GetCoinsUseCaseTest.kt
+++ app/src/test/java/me/juangoncalves/mentra/features/wallet_management/domain/usecases/GetCoinsUseCaseTest.kt
@@ -2,7 +2,9 @@ package me.juangoncalves.mentra.features.wallet_management.domain.usecases
 
 import either.Either
 import kotlinx.coroutines.runBlocking
-import me.juangoncalves.mentra.features.wallet_management.domain.entities.Coin
+import me.juangoncalves.mentra.features.wallet_management.Bitcoin
+import me.juangoncalves.mentra.features.wallet_management.Ethereum
+import me.juangoncalves.mentra.features.wallet_management.Ripple
 import me.juangoncalves.mentra.features.wallet_management.domain.repositories.CoinRepository
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -23,11 +25,7 @@ class GetCoinsUseCaseTest {
     @Test
     fun `should get coin list from the repository`() = runBlocking<Unit> {
         // Arrange
-        val coins = listOf(
-            Coin(name = "Bitcoin", symbol = "BTC", imageUrl = "http://url.com/btc.jpg"),
-            Coin(name = "Ethereum", symbol = "ETH", imageUrl = "http://url.com/eth.jpg"),
-            Coin(name = "Ripple", symbol = "XRP", imageUrl = "http://url.com/xrp.jpg")
-        )
+        val coins = listOf(Bitcoin, Ethereum, Ripple)
         `when`(coinRepositoryMock.getCoins()).thenReturn(Either.Right(coins))
 
         // Act
