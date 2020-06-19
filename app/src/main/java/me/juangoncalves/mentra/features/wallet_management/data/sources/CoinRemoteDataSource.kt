baed7880e2395a10fe806be14530cc3c8b237f67
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-18 21:52:57 -0400

    Define the interface for the coin remote sources

---
+++ app/src/main/java/me/juangoncalves/mentra/features/wallet_management/data/sources/CoinRemoteDataSource.kt
@@ -0,0 +1,28 @@
+package me.juangoncalves.mentra.features.wallet_management.data.sources
+
+import me.juangoncalves.mentra.core.errors.NotFoundException
+import me.juangoncalves.mentra.core.errors.ServerException
+import me.juangoncalves.mentra.features.wallet_management.data.models.CoinSchema
+import me.juangoncalves.mentra.features.wallet_management.domain.entities.Coin
+import me.juangoncalves.mentra.features.wallet_management.domain.entities.Money
+
+interface CoinRemoteDataSource {
+
+    /**
+     * Fetches the list of supported coins from a remote data source like a server or a
+     * third party API.
+     *
+     * @throws ServerException for all error codes.
+     */
+    suspend fun fetchCoins(): List<CoinSchema>
+
+    /**
+     * Fetches the current price of a single coin from a remote data source like a server or a
+     * third party API.
+     *
+     * @throws NotFoundException when the coin price is not available in the remote source.
+     * @throws ServerException for all error codes.
+     */
+    suspend fun fetchCoinPrice(coin: Coin): Money
+
+}
