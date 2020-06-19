97de39358e941e2c4666383f4b992ff35ca4a50e
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-18 21:51:32 -0400

    Create exceptions for the data layer

---
+++ app/src/main/java/me/juangoncalves/mentra/core/errors/Exceptions.kt
@@ -0,0 +1,8 @@
+package me.juangoncalves.mentra.core.errors
+
+
+class ServerException : RuntimeException()
+
+class NotFoundException : RuntimeException()
+
+class LocalDataSourceException : RuntimeException()
