78dee6c38fa27eb6857c2ab9e4897730a76d618c
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-26 09:24:02 -0400

    Add date attribute to the `Price` class
    This allows knowing how recent the price is to determine if
    a fetch on the remote data source should be done.

--- app/src/main/java/me/juangoncalves/mentra/features/wallet_management/domain/entities/Price.kt
+++ app/src/main/java/me/juangoncalves/mentra/features/wallet_management/domain/entities/Price.kt
@@ -1,6 +1,9 @@
 package me.juangoncalves.mentra.features.wallet_management.domain.entities
 
+import java.util.*
+
 data class Price(
     val currency: Currency,
-    val value: Double
+    val value: Double,
+    val date: Date
 )
