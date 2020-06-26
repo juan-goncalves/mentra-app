71c63753f7432738699f5a71654f9b31154e85c4
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-26 09:29:09 -0400

    Replace `mockito` with `mockk`

--- app/src/test/java/me/juangoncalves/mentra/features/wallet_management/domain/usecases/GetCoinPriceUseCaseTest.kt
+++ app/src/test/java/me/juangoncalves/mentra/features/wallet_management/domain/usecases/GetCoinPriceUseCaseTest.kt
@@ -1,10 +1,15 @@
 package me.juangoncalves.mentra.features.wallet_management.domain.usecases
 
 import either.Either
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.runBlocking
 import me.juangoncalves.mentra.core.errors.PriceNotFound
 import me.juangoncalves.mentra.features.wallet_management.Bitcoin
 import me.juangoncalves.mentra.features.wallet_management.Ethereum
+import me.juangoncalves.mentra.features.wallet_management.Left
+import me.juangoncalves.mentra.features.wallet_management.Right
 import me.juangoncalves.mentra.features.wallet_management.domain.entities.Currency
 import me.juangoncalves.mentra.features.wallet_management.domain.entities.Price
 import me.juangoncalves.mentra.features.wallet_management.domain.repositories.CoinRepository
@@ -12,30 +17,33 @@ import org.hamcrest.Matchers.closeTo
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.*
+import java.util.*
 
 class GetCoinPriceUseCaseTest {
 
-    private lateinit var coinRepositoryMock: CoinRepository
+    @MockK
+    lateinit var coinRepositoryMock: CoinRepository
+
     private lateinit var useCase: GetCoinPriceUseCase
 
     @Before
     fun setUp() {
-        coinRepositoryMock = mock(CoinRepository::class.java)
+        MockKAnnotations.init(this)
         useCase = GetCoinPriceUseCase(coinRepositoryMock)
     }
 
     @Test
     fun `should return the price of the selected coin from the repository`() = runBlocking {
         // Arrange
-        val dataStub = Either.Right(Price(Currency.USD, 9834.23))
-        `when`(coinRepositoryMock.getCoinPrice(Bitcoin, Currency.USD)).thenReturn(dataStub)
+        val dataStub = Right(Price(Currency.USD, 9834.23, Date()))
+        coEvery { coinRepositoryMock.getCoinPrice(Bitcoin, Currency.USD) } returns dataStub
 
         // Act
-        val result = useCase.execute(Bitcoin) as Either.Right
+        val result = useCase.execute(Bitcoin)
 
         // Assert
-        assertThat(result.value.value, closeTo(9834.23, 0.001))
+        val resultData = (result as Right).value
+        assertThat(resultData.value, closeTo(9834.23, 0.001))
         assertEquals(Currency.USD, result.value.currency)
     }
 
@@ -43,12 +51,7 @@ class GetCoinPriceUseCaseTest {
     fun `should fail if the price of the selected coin is not found`() = runBlocking {
         // Arrange
         val failure = PriceNotFound(Ethereum)
-        `when`(
-            coinRepositoryMock.getCoinPrice(
-                eq(Ethereum),
-                any()
-            )
-        ).thenReturn(Either.Left(failure))
+        coEvery { coinRepositoryMock.getCoinPrice(Ethereum, any()) } returns Left(failure)
 
         // Act
         val result = useCase.execute(Ethereum) as Either.Left
