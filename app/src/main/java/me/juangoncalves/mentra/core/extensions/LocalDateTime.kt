c3e2e42677ff6bf9955bc90261d3d6511b6bba18
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-27 10:32:54 -0400

    Implement extension function to get the amount of minutes between two LocalDateTime

---
+++ app/src/main/java/me/juangoncalves/mentra/core/extensions/LocalDateTime.kt
@@ -0,0 +1,11 @@
+package me.juangoncalves.mentra.core.extensions
+
+import java.time.Duration
+import java.time.LocalDateTime
+import kotlin.math.abs
+
+
+fun LocalDateTime.minutesBetween(other: LocalDateTime): Long {
+    val duration = Duration.between(this, other)
+    return abs(duration.toMinutes())
+}
