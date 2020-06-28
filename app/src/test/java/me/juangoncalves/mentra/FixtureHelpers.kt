edadf92b58ebbb82119246476767bc61e812caff
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-28 11:15:24 -0400

    Add a lazily instantiated `Moshi` object to the fixture helpers
    This object will be used to parse every JSON fixture into the corresponding schema class

--- app/src/test/java/me/juangoncalves/mentra/FixtureHelpers.kt
+++ app/src/test/java/me/juangoncalves/mentra/FixtureHelpers.kt
@@ -1,6 +1,9 @@
 package me.juangoncalves.mentra
 
+import com.squareup.moshi.Moshi
 import java.io.File
+
+val moshi: Moshi by lazy { Moshi.Builder().build() }
 
 fun Any.fixture(path: String): String {
     val url = this.javaClass.getResource(path)
