ad26cc262533a05f53ca578bef6865886a538ca5
Juan Goncalves <juan.goncalves498@gmail.com>
2020-06-28 11:13:47 -0400

    Add message to the ServerException constructor
    As its used in different situations we should add a message to quickly identify which case happened

--- app/src/main/java/me/juangoncalves/mentra/core/errors/Exceptions.kt
+++ app/src/main/java/me/juangoncalves/mentra/core/errors/Exceptions.kt
@@ -3,7 +3,7 @@ package me.juangoncalves.mentra.core.errors
 import me.juangoncalves.mentra.features.portfolio.domain.entities.Price
 
 
-class ServerException : RuntimeException()
+class ServerException(message: String = "") : RuntimeException(message)
 
 class NotFoundException : RuntimeException()
 
