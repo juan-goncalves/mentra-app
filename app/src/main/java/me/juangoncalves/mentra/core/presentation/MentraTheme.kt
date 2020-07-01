9bd83bfa90f2e2a06c2cef293e09ff74a7783520
Juan Goncalves <juan.goncalves498@gmail.com>
2020-07-01 08:56:27 -0400

    Create a composable for the app's theme

---
+++ app/src/main/java/me/juangoncalves/mentra/core/presentation/MentraTheme.kt
@@ -0,0 +1,35 @@
+package me.juangoncalves.mentra.core.presentation
+
+import androidx.compose.Composable
+import androidx.ui.foundation.isSystemInDarkTheme
+import androidx.ui.graphics.Color
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.darkColorPalette
+import androidx.ui.material.lightColorPalette
+
+
+val LightColorPalette = lightColorPalette(
+    primary = Color(0x87, 0x76, 0xE5),
+    primaryVariant = Color(0xFF, 0x36, 0x81),
+    onPrimary = Color(0xFF, 0xFF, 0xFF)
+)
+
+val DarkColorPalette = darkColorPalette(
+    primary = Color(0x87, 0x76, 0xE5),
+    primaryVariant = Color(0xFF, 0x36, 0x81),
+    surface = Color(0x34, 0x3E, 0x5C),
+    onPrimary = Color(0xFF, 0xFF, 0xFF)
+)
+
+@Composable
+fun MentraTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colorPalette = if (darkTheme) DarkColorPalette else LightColorPalette
+    MaterialTheme(
+        colors = colorPalette
+    ) {
+        content()
+    }
+}
