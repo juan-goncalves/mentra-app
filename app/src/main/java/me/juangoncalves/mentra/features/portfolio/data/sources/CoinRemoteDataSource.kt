c6bed8a9b6fec127b630ceaeda393aa27ad3d0e6
Juan Goncalves <juan.goncalves498@gmail.com>
2020-07-07 20:58:21 -0400

    Update documentation

--- app/src/main/java/me/juangoncalves/mentra/features/portfolio/data/sources/CoinRemoteDataSource.kt
+++ app/src/main/java/me/juangoncalves/mentra/features/portfolio/data/sources/CoinRemoteDataSource.kt
@@ -1,5 +1,6 @@
 package me.juangoncalves.mentra.features.portfolio.data.sources
 
+import me.juangoncalves.mentra.core.errors.InternetConnectionException
 import me.juangoncalves.mentra.core.errors.ServerException
 import me.juangoncalves.mentra.features.portfolio.data.schemas.CoinSchema
 import me.juangoncalves.mentra.features.portfolio.domain.entities.Coin
@@ -12,6 +13,7 @@ interface CoinRemoteDataSource {
      * third party API.
      *
      * @throws ServerException for all error codes.
+     * @throws InternetConnectionException if the communication with the remote source fails.
      */
     suspend fun fetchCoins(): List<CoinSchema>
 
@@ -20,6 +22,7 @@ interface CoinRemoteDataSource {
      * or a third party API.
      *
      * @throws ServerException for all error codes.
+     * @throws InternetConnectionException if the communication with the remote source fails.
      */
     suspend fun fetchCoinPrice(coin: Coin): Price
 
