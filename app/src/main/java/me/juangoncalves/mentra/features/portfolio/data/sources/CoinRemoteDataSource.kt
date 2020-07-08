e022d8644e60fe10c87cb8e1623e60f9ccf1a93f
Juan Goncalves <juan.goncalves498@gmail.com>
2020-07-07 20:45:15 -0400

    Remove the currency parameter and update documentation
    The API will always return the prices in USD, afterwards we will have to convert them to other currencies using some other API.

--- app/src/main/java/me/juangoncalves/mentra/features/portfolio/data/sources/CoinRemoteDataSource.kt
+++ app/src/main/java/me/juangoncalves/mentra/features/portfolio/data/sources/CoinRemoteDataSource.kt
@@ -1,10 +1,8 @@
 package me.juangoncalves.mentra.features.portfolio.data.sources
 
-import me.juangoncalves.mentra.core.errors.NotFoundException
 import me.juangoncalves.mentra.core.errors.ServerException
 import me.juangoncalves.mentra.features.portfolio.data.schemas.CoinSchema
 import me.juangoncalves.mentra.features.portfolio.domain.entities.Coin
-import me.juangoncalves.mentra.features.portfolio.domain.entities.Currency
 import me.juangoncalves.mentra.features.portfolio.domain.entities.Price
 
 interface CoinRemoteDataSource {
@@ -18,12 +16,11 @@ interface CoinRemoteDataSource {
     suspend fun fetchCoins(): List<CoinSchema>
 
     /**
-     * Fetches the current price of a single coin from a remote data source like a server or a
-     * third party API.
+     * Fetches the current price in USD of a single coin from a remote data source like a server
+     * or a third party API.
      *
-     * @throws NotFoundException when the coin price is not available in the remote source.
      * @throws ServerException for all error codes.
      */
-    suspend fun fetchCoinPrice(coin: Coin, currency: Currency): Price
+    suspend fun fetchCoinPrice(coin: Coin): Price
 
 }
