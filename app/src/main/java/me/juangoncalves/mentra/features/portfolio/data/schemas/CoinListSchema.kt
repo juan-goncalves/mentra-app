6821991e371c6f2df953288c187962843ebde45a
Juan Goncalves <juan.goncalves498@gmail.com>
2020-07-07 20:42:31 -0400

    Update the coin list schema to use the generic api response type

--- app/src/main/java/me/juangoncalves/mentra/features/portfolio/data/schemas/CoinListSchema.kt
+++ app/src/main/java/me/juangoncalves/mentra/features/portfolio/data/schemas/CoinListSchema.kt
@@ -1,13 +1,3 @@
 package me.juangoncalves.mentra.features.portfolio.data.schemas
 
-import com.squareup.moshi.Json
-
-
-data class CoinListSchema(
-    @field:Json(name = "BaseImageUrl")
-    val baseImageUrl: String = "",
-    @field:Json(name = "BaseLinkUrl")
-    val baseLinkUrl: String = "",
-    @field:Json(name = "Data")
-    val data: Map<String, CoinSchema> = mapOf()
-)
+typealias CoinListSchema = CryptoCompareResponse<Map<String, CoinSchema>>
