2d562e51f2675beaa84167dff7001855a9c56ddc
Juan Goncalves <juan.goncalves498@gmail.com>
2020-07-12 10:46:53 -0400

    Display the app activities in full screen with transparent navigation and status bars

--- app/src/main/java/me/juangoncalves/mentra/MentraApplication.kt
+++ app/src/main/java/me/juangoncalves/mentra/MentraApplication.kt
@@ -2,6 +2,14 @@ package me.juangoncalves.mentra
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import me.juangoncalves.mentra.core.presentation.WindowLayoutCallbacks
 
 @HiltAndroidApp
-class MentraApplication : Application()
+class MentraApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(WindowLayoutCallbacks())
+    }
+
+}
