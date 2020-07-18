d42353c67ed017ceecc413fac111856e8f331bce
Juan Goncalves <juan.goncalves498@gmail.com>
2020-07-18 11:08:13 -0400

    Inject dependencies with dagger

--- app/src/main/java/me/juangoncalves/mentra/domain/usecases/GetCoinPriceUseCase.kt
+++ app/src/main/java/me/juangoncalves/mentra/domain/usecases/GetCoinPriceUseCase.kt
@@ -6,8 +6,11 @@ import me.juangoncalves.mentra.domain.models.Coin
 import me.juangoncalves.mentra.domain.models.Currency
 import me.juangoncalves.mentra.domain.models.Price
 import me.juangoncalves.mentra.domain.repositories.CoinRepository
+import javax.inject.Inject
 
-class GetCoinPriceUseCase(private val coinRepository: CoinRepository) {
+class GetCoinPriceUseCase @Inject constructor(
+    private val coinRepository: CoinRepository
+) {
 
     suspend operator fun invoke(
         coin: Coin,
--- app/src/main/java/me/juangoncalves/mentra/domain/usecases/GetWalletsUseCase.kt
+++ app/src/main/java/me/juangoncalves/mentra/domain/usecases/GetWalletsUseCase.kt
@@ -4,8 +4,11 @@ import either.Either
 import me.juangoncalves.mentra.domain.errors.Failure
 import me.juangoncalves.mentra.domain.models.Wallet
 import me.juangoncalves.mentra.domain.repositories.WalletRepository
+import javax.inject.Inject
 
-class GetWalletsUseCase(private val walletRepository: WalletRepository) {
+class GetWalletsUseCase @Inject constructor(
+    private val walletRepository: WalletRepository
+) {
 
     suspend operator fun invoke(): Either<Failure, List<Wallet>> = walletRepository.getWallets()
 
