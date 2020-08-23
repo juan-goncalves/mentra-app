45f6bce3feb48db5556672a9017e2ff0b13b5697
Juan Goncalves <juan.goncalves498@gmail.com>
2020-08-22 23:32:01 -0400

    Create extension function to update a view's visibility with a boolean

--- app/src/main/java/me/juangoncalves/mentra/extensions/View.kt
+++ app/src/main/java/me/juangoncalves/mentra/extensions/View.kt
@@ -3,9 +3,13 @@ package me.juangoncalves.mentra.extensions
 import android.view.View
 
 fun View.hide() {
-    visibility = View.GONE
+    visibility = View.INVISIBLE
 }
 
 fun View.show() {
     visibility = View.VISIBLE
+}
+
+fun View.updateVisibility(shouldShow: Boolean) {
+    if (shouldShow) show() else hide()
 }
