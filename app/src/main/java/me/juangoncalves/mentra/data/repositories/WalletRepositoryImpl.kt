1030f7c9662bc3b0bc979fc9d01161f09570e23e
Juan Goncalves <juan.goncalves498@gmail.com>
2020-09-17 23:20:54 -0400

    Extract repeated code into a helper function

--- app/src/main/java/me/juangoncalves/mentra/data/repositories/WalletRepositoryImpl.kt
+++ app/src/main/java/me/juangoncalves/mentra/data/repositories/WalletRepositoryImpl.kt
@@ -4,14 +4,12 @@ import either.Either
 import me.juangoncalves.mentra.data.mapper.WalletMapper
 import me.juangoncalves.mentra.data.sources.wallet.WalletLocalDataSource
 import me.juangoncalves.mentra.domain.errors.Failure
-import me.juangoncalves.mentra.domain.errors.StorageException
 import me.juangoncalves.mentra.domain.errors.StorageFailure
 import me.juangoncalves.mentra.domain.models.Coin
 import me.juangoncalves.mentra.domain.models.Currency
 import me.juangoncalves.mentra.domain.models.Price
 import me.juangoncalves.mentra.domain.models.Wallet
 import me.juangoncalves.mentra.domain.repositories.WalletRepository
-import me.juangoncalves.mentra.extensions.Left
 import me.juangoncalves.mentra.extensions.Right
 import me.juangoncalves.mentra.extensions.TAG
 import me.juangoncalves.mentra.log.Logger
@@ -23,58 +21,45 @@ class WalletRepositoryImpl @Inject constructor(
     private val logger: Logger
 ) : WalletRepository {
 
-    override suspend fun getWallets(): Either<Failure, List<Wallet>> {
-        return try {
-            val models = localDataSource.getAll()
-            val wallets = models.map { walletMapper.map(it) }
-            Either.Right(wallets)
-        } catch (e: StorageException) {
-            logger.error(TAG, "Error communicating with the local database.\n$$e")
-            Either.Left(StorageFailure())
-        }
+    override suspend fun getWallets(): Either<Failure, List<Wallet>> = handleException {
+        val models = localDataSource.getAll()
+        val wallets = models.map { walletMapper.map(it) }
+        Either.Right(wallets)
     }
 
-    override suspend fun createWallet(wallet: Wallet): Either<Failure, Unit> {
-        return try {
-            val model = walletMapper.map(wallet)
-            localDataSource.save(model)
-            Either.Right(Unit)
-        } catch (e: StorageException) {
-            logger.error(TAG, "Error communicating with the local database.\n$$e")
-            Either.Left(StorageFailure())
-        }
+    override suspend fun createWallet(wallet: Wallet): Either<Failure, Unit> = handleException {
+        val model = walletMapper.map(wallet)
+        localDataSource.save(model)
+        Either.Right(Unit)
     }
 
-    override suspend fun findWalletsByCoin(coin: Coin): Either<Failure, List<Wallet>> {
-        return try {
+    override suspend fun findWalletsByCoin(coin: Coin): Either<Failure, List<Wallet>> =
+        handleException {
             val models = localDataSource.findByCoin(coin)
             val wallets = models.map { walletMapper.map(it) }
             Either.Right(wallets)
-        } catch (e: StorageException) {
-            logger.error(TAG, "Error communicating with the local database.\n$$e")
-            Either.Left(StorageFailure())
         }
-    }
 
-    override suspend fun updateWalletValue(wallet: Wallet, price: Price): Either<Failure, Unit> {
-        return try {
+    override suspend fun updateWalletValue(wallet: Wallet, price: Price): Either<Failure, Unit> =
+        handleException {
             localDataSource.updateValue(wallet, price)
             Either.Right(Unit)
-        } catch (e: StorageException) {
-            logger.error(TAG, "Error communicating with the local database.\n$$e")
-            Either.Left(StorageFailure())
         }
-    }
 
-    override suspend fun getWalletValueHistory(wallet: Wallet): Either<Failure, List<Price>> {
-        return try {
+    override suspend fun getWalletValueHistory(wallet: Wallet): Either<Failure, List<Price>> =
+        handleException {
             val prices = localDataSource.getValueHistory(wallet).map { valueModel ->
                 Price(Currency.USD, valueModel.valueInUSD, valueModel.date.atStartOfDay())
             }
             Right(prices)
-        } catch (e: StorageException) {
+        }
+
+    private suspend fun <R> handleException(source: suspend () -> Right<R>): Either<Failure, R> {
+        return try {
+            source.invoke()
+        } catch (e: Exception) {
             logger.error(TAG, "Error communicating with the local database.\n$$e")
-            Left(StorageFailure())
+            Either.Left(StorageFailure())
         }
     }
 
