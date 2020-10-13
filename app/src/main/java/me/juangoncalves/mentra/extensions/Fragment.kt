63b24e08d171333e4ddd3a13d6b0144d7490f700
Juan Goncalves <juan.goncalves498@gmail.com>
2020-10-12 20:26:55 -0400

    Merge the error snackbar creation overloads using default arguments Implement an extension function to easily handle the default errors from a view model

--- app/src/main/java/me/juangoncalves/mentra/extensions/Fragment.kt
+++ app/src/main/java/me/juangoncalves/mentra/extensions/Fragment.kt
@@ -2,18 +2,37 @@ package me.juangoncalves.mentra.extensions
 
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.observe
 import com.google.android.material.snackbar.Snackbar
 import me.juangoncalves.mentra.R
+import me.juangoncalves.mentra.ui.common.DefaultErrorHandlingViewModel
 import me.juangoncalves.mentra.ui.common.DisplayError
 
-fun Fragment.createErrorSnackbar(error: DisplayError, anchor: View = requireView()): Snackbar {
+fun Fragment.createErrorSnackbar(
+    error: DisplayError,
+    anchor: View? = null,
+    duration: Int = Snackbar.LENGTH_INDEFINITE
+): Snackbar {
     val errorColor = requireContext().getThemeColor(R.attr.colorError)
     val onErrorColor = requireContext().getThemeColor(R.attr.colorOnError)
 
-    return Snackbar.make(anchor, error.messageId, Snackbar.LENGTH_LONG)
+    return Snackbar.make(requireView(), error.messageId, duration)
         .setBackgroundTint(errorColor)
         .setTextColor(onErrorColor)
         .setAction(R.string.retry) { error.retryAction() }
-        .setAnchorView(anchor)
         .setActionTextColor(onErrorColor)
+        .apply {
+            if (anchor != null) anchorView = anchor
+        }
+}
+
+fun Fragment.showSnackbarOnDefaultErrors(
+    viewModel: DefaultErrorHandlingViewModel,
+    anchor: View? = null
+) {
+    viewModel.defaultErrorStream.observe(viewLifecycleOwner) { error ->
+        createErrorSnackbar(error, anchor)
+            .onDismissed { viewModel.errorSnackbarDismissed() }
+            .show()
+    }
 }
