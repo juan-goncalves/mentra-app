05d83bb52baa506a5c5af237f1ac14b2177eeb3d
Juan Goncalves <juan.goncalves498@gmail.com>
2020-10-12 21:39:40 -0400

    Implement use case to create wallets

--- app/src/main/java/me/juangoncalves/mentra/domain/usecases/wallet/CreateWallet.kt
+++ app/src/main/java/me/juangoncalves/mentra/domain/usecases/wallet/CreateWallet.kt
@@ -4,13 +4,15 @@ import either.Either
 import me.juangoncalves.mentra.domain.errors.Failure
 import me.juangoncalves.mentra.domain.models.Wallet
 import me.juangoncalves.mentra.domain.repositories.WalletRepository
+import me.juangoncalves.mentra.domain.usecases.UseCase
 import javax.inject.Inject
 
-class CreateWalletUseCase @Inject constructor(
+class CreateWallet @Inject constructor(
     private val walletRepository: WalletRepository
-) {
+) : UseCase<Wallet, Unit> {
 
-    suspend operator fun invoke(wallet: Wallet): Either<Failure, Unit> =
-        walletRepository.createWallet(wallet)
+    override suspend operator fun invoke(params: Wallet): Either<Failure, Unit> {
+        return walletRepository.createWallet(params)
+    }
 
 }
