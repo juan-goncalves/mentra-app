a6891b6b7b1bd21382730a425d6e2b55a6b0966e
Juan Goncalves <juan.goncalves498@gmail.com>
2020-10-22 11:14:27 -0400

    Implement function to easily compare doubles

--- pie/src/main/java/me/juangoncalves/pie/extensions/Double.kt
+++ pie/src/main/java/me/juangoncalves/pie/extensions/Double.kt
@@ -1,6 +1,7 @@
 package me.juangoncalves.pie.extensions
 
 import java.text.DecimalFormat
+import kotlin.math.abs
 
 internal fun Double.asPercentage(): String {
     val format = DecimalFormat()
@@ -8,3 +9,5 @@ internal fun Double.asPercentage(): String {
     format.maximumFractionDigits = 2
     return format.format(this * 100) + "%"
 }
+
+internal infix fun Double.closeTo(other: Double): Boolean = abs(this - other) <= 0.001
