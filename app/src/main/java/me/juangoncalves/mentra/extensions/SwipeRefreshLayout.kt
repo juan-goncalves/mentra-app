17f60a6b09ab54e23c795f03e438c7a48110dbc8
Juan Goncalves <juan.goncalves498@gmail.com>
2020-10-22 10:31:02 -0400

    Implement extension function to style our refresh layouts

---
+++ app/src/main/java/me/juangoncalves/mentra/extensions/SwipeRefreshLayout.kt
@@ -0,0 +1,13 @@
+package me.juangoncalves.mentra.extensions
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import me.juangoncalves.mentra.R
+
+fun SwipeRefreshLayout.styleByTheme() = context?.let { context ->
+    setColorSchemeColors(
+        context.getThemeColor(R.attr.colorSecondary),
+        context.getThemeColor(R.attr.colorSecondaryVariant),
+        context.getThemeColor(R.attr.colorPrimary),
+        context.getThemeColor(R.attr.colorPrimaryDark)
+    )
+}
