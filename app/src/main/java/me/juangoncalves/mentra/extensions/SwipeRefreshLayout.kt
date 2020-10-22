0d3606283134cb43c83b8e08e65748e1d31165be
Juan Goncalves <juan.goncalves498@gmail.com>
2020-10-22 10:44:05 -0400

    Make the `styleByTheme` function return the same SwipeRefreshLayout
    This allows chaining calls in a builder pattern fashion

--- app/src/main/java/me/juangoncalves/mentra/extensions/SwipeRefreshLayout.kt
+++ app/src/main/java/me/juangoncalves/mentra/extensions/SwipeRefreshLayout.kt
@@ -3,11 +3,13 @@ package me.juangoncalves.mentra.extensions
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import me.juangoncalves.mentra.R
 
-fun SwipeRefreshLayout.styleByTheme() = context?.let { context ->
-    setColorSchemeColors(
-        context.getThemeColor(R.attr.colorSecondary),
-        context.getThemeColor(R.attr.colorSecondaryVariant),
-        context.getThemeColor(R.attr.colorPrimary),
-        context.getThemeColor(R.attr.colorPrimaryDark)
-    )
+fun SwipeRefreshLayout.styleByTheme() = apply {
+    context?.let { context ->
+        setColorSchemeColors(
+            context.getThemeColor(R.attr.colorSecondary),
+            context.getThemeColor(R.attr.colorSecondaryVariant),
+            context.getThemeColor(R.attr.colorPrimary),
+            context.getThemeColor(R.attr.colorPrimaryDark)
+        )
+    }
 }
