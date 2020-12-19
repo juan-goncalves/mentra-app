3648ec542131f526d6afa095f89b76e07cb8ce30
Juan Goncalves <juan.goncalves498@gmail.com>
2020-12-18 20:33:34 -0400

    Refactor the `IconRepository`

--- data-layer/src/main/java/me/juangoncalves/mentra/data_layer/repositories/IconRepositoryImpl.kt
+++ data-layer/src/main/java/me/juangoncalves/mentra/data_layer/repositories/IconRepositoryImpl.kt
@@ -1,30 +1,23 @@
 package me.juangoncalves.mentra.data_layer.repositories
 
 import either.Either
+import kotlinx.coroutines.Dispatchers
 import me.juangoncalves.mentra.data_layer.sources.coin.CoinIconDataSource
-import me.juangoncalves.mentra.domain_layer.errors.OldFailure
-import me.juangoncalves.mentra.domain_layer.extensions.Left
-import me.juangoncalves.mentra.domain_layer.extensions.Right
-import me.juangoncalves.mentra.domain_layer.extensions.TAG
-import me.juangoncalves.mentra.domain_layer.log.MentraLogger
+import me.juangoncalves.mentra.domain_layer.errors.ErrorHandler
+import me.juangoncalves.mentra.domain_layer.errors.Failure
+import me.juangoncalves.mentra.domain_layer.errors.runCatching
 import me.juangoncalves.mentra.domain_layer.models.Coin
 import me.juangoncalves.mentra.domain_layer.repositories.IconRepository
 import javax.inject.Inject
 
 class IconRepositoryImpl @Inject constructor(
     private val iconDataSource: CoinIconDataSource,
-    private val logger: MentraLogger
+    private val errorHandler: ErrorHandler
 ) : IconRepository {
 
-    override suspend fun getAlternativeIconFor(coin: Coin): Either<OldFailure, String?> {
-        // TODO: We should use a ErrorHandler
-        return try {
-            val iconUrl = iconDataSource.getAlternativeIconFor(coin)
-            Right(iconUrl)
-        } catch (e: Exception) {
-            logger.warning(TAG, "Can't to determine if a gradient icon is available for coin")
-            Left(OldFailure())
+    override suspend fun getAlternativeIconFor(coin: Coin): Either<Failure, String?> =
+        errorHandler.runCatching(Dispatchers.IO) {
+            iconDataSource.getAlternativeIconFor(coin)
         }
-    }
 
 }
--- domain-layer/src/main/java/me/juangoncalves/mentra/domain_layer/repositories/IconRepository.kt
+++ domain-layer/src/main/java/me/juangoncalves/mentra/domain_layer/repositories/IconRepository.kt
@@ -1,11 +1,11 @@
 package me.juangoncalves.mentra.domain_layer.repositories
 
 import either.Either
-import me.juangoncalves.mentra.domain_layer.errors.OldFailure
+import me.juangoncalves.mentra.domain_layer.errors.Failure
 import me.juangoncalves.mentra.domain_layer.models.Coin
 
 interface IconRepository {
 
-    suspend fun getAlternativeIconFor(coin: Coin): Either<OldFailure, String?>
+    suspend fun getAlternativeIconFor(coin: Coin): Either<Failure, String?>
 
 }
