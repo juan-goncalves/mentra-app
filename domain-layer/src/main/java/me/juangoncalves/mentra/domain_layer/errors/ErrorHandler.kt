1e2385fdd7bbb72eeb99f224d75e314d5f3d85a5
Juan Goncalves <juan.goncalves498@gmail.com>
2020-12-31 10:04:41 -0400

    Document the `ErrorHandler` interface and extension methods

--- domain-layer/src/main/java/me/juangoncalves/mentra/domain_layer/errors/ErrorHandler.kt
+++ domain-layer/src/main/java/me/juangoncalves/mentra/domain_layer/errors/ErrorHandler.kt
@@ -9,10 +9,22 @@ import kotlin.coroutines.CoroutineContext
 
 interface ErrorHandler {
 
+    /**
+     * Handles the processing of the [Throwable] (side effects, logging, etc) and returns the
+     * corresponding [Failure].
+     * */
     fun getFailure(throwable: Throwable): Failure
 
 }
 
+/**
+ * Executes the [block] handling any [Exception]s that are thrown except [CancellationException]
+ * to avoid interrupting the coroutine cancellation chain, wrapping the result in a [Either].
+ *
+ * See [ErrorHandler.ignoringFailure] which supplements this method.
+ *
+ * @return [Either] with the result of the [block] or a [Failure]
+ * */
 suspend inline fun <T> ErrorHandler.runCatching(
     context: CoroutineContext? = null,
     crossinline block: suspend ErrorHandler.() -> T
@@ -36,6 +48,15 @@ suspend inline fun <T> ErrorHandler.runCatching(
     }
 }
 
+/**
+ * Executes the [block] handling any [Exception]s that are thrown except [CancellationException]
+ * to avoid interrupting the coroutine cancellation chain.
+ *
+ * It's main use case is to execute operations or side effects that shouldn't interrupt the main
+ * work if they fail.
+ *
+ * @return the result of the [block] if no exceptions where caught, else null
+ * */
 suspend inline fun <T> ErrorHandler.ignoringFailure(
     context: CoroutineContext? = null,
     crossinline block: suspend ErrorHandler.() -> T
@@ -44,8 +65,12 @@ suspend inline fun <T> ErrorHandler.ignoringFailure(
         try {
             block()
         } catch (e: Exception) {
-            getFailure(e)
-            null
+            if (e is CancellationException) {
+                throw e
+            } else {
+                getFailure(e)
+                null
+            }
         }
     }
 
