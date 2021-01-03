fe36abcd18b4fe7058264b5a8dc5643eb24455ec
Juan Goncalves <juan.goncalves498@gmail.com>
2021-01-03 09:43:56 -0400

    Remove unused error stream from the `DashboardViewModel`

--- app/src/main/java/me/juangoncalves/mentra/extensions/Context.kt
+++ app/src/main/java/me/juangoncalves/mentra/extensions/Context.kt
@@ -2,34 +2,10 @@ package me.juangoncalves.mentra.extensions
 
 import android.content.Context
 import android.util.TypedValue
-import android.view.View
 import androidx.annotation.AttrRes
-import com.google.android.material.snackbar.Snackbar
-import me.juangoncalves.mentra.R
-import me.juangoncalves.mentra.features.common.DisplayError
 
 fun Context.getThemeColor(@AttrRes attrRes: Int): Int {
     val typedValue = TypedValue()
     theme.resolveAttribute(attrRes, typedValue, true)
     return typedValue.data
-}
-
-
-fun Context.createErrorSnackbar(
-    error: DisplayError,
-    view: View,
-    duration: Int = Snackbar.LENGTH_INDEFINITE,
-    anchor: View? = null
-): Snackbar {
-    val errorColor = getThemeColor(R.attr.colorError)
-    val onErrorColor = getThemeColor(R.attr.colorOnError)
-
-    return Snackbar.make(view, error.messageId, duration)
-        .setBackgroundTint(errorColor)
-        .setTextColor(onErrorColor)
-        .setAction(R.string.retry) { error.retryAction() }
-        .setActionTextColor(onErrorColor)
-        .apply {
-            if (anchor != null) anchorView = anchor
-        }
 }
