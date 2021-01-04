4798cc7e84d1bd11ea7cbe2d0b8d6a1d71a33d7d
Juan Goncalves <juan.goncalves498@gmail.com>
2021-01-04 13:53:30 -0400

    Create package for the benefits onboarding step

--- app/src/main/java/me/juangoncalves/mentra/features/onboarding/OnboardingStepsAdapter.kt
+++ app/src/main/java/me/juangoncalves/mentra/features/onboarding/OnboardingStepsAdapter.kt
@@ -3,6 +3,7 @@ package me.juangoncalves.mentra.features.onboarding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import me.juangoncalves.mentra.features.onboarding.benefits.OnboardingBenefitsFragment
 import me.juangoncalves.mentra.features.onboarding.currency.OnboardingCurrencyFragment
 import me.juangoncalves.mentra.features.onboarding.periodic_refresh.OnboardingAutoRefreshFragment
 
--- app/src/main/java/me/juangoncalves/mentra/features/onboarding/benefits/OnboardingBenefitsFragment.kt
+++ app/src/main/java/me/juangoncalves/mentra/features/onboarding/benefits/OnboardingBenefitsFragment.kt
@@ -1,4 +1,4 @@
-package me.juangoncalves.mentra.features.onboarding
+package me.juangoncalves.mentra.features.onboarding.benefits
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import me.juangoncalves.mentra.databinding.OnboardingBenefitsFragmentBinding
+import me.juangoncalves.mentra.features.onboarding.OnboardingViewModel
 
 
 class OnboardingBenefitsFragment : Fragment() {
