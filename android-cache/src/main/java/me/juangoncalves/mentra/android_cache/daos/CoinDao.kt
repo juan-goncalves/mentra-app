32c0a22b6a597242e0ab68a8b695c6a494256bed
Juan Goncalves <juan.goncalves498@gmail.com>
2022-02-26 10:31:25 -0400

    Save the sort order of each coin in the database

--- android-cache/src/main/java/me/juangoncalves/mentra/android_cache/daos/CoinDao.kt
+++ android-cache/src/main/java/me/juangoncalves/mentra/android_cache/daos/CoinDao.kt
@@ -6,19 +6,26 @@ import me.juangoncalves.mentra.android_cache.models.CoinModel
 @Dao
 interface CoinDao {
 
-    @Query("SELECT * FROM Coin")
+    @Query("SELECT * FROM Coin ORDER BY position ASC")
     suspend fun getAll(): List<CoinModel>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(vararg coins: CoinModel)
 
     @Query("DELETE FROM Coin")
     suspend fun clearAll()
 
-    @Query("SELECT * FROM Coin WHERE symbol = :symbol")
+    @Query("SELECT * FROM Coin WHERE symbol = :symbol ORDER BY position ASC")
     suspend fun getCoinBySymbol(symbol: String): CoinModel?
 
     @Update
     suspend fun update(coin: CoinModel)
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(coin: CoinModel): Long
+
+    @Transaction
+    suspend fun upsertAll(vararg coins: CoinModel) {
+        coins.forEach { coin ->
+            val rowId = insert(coin)
+            if (rowId == -1L) update(coin)
+        }
+    }
 }
--- android-cache/src/main/java/me/juangoncalves/mentra/android_cache/mappers/CoinMapper.kt
+++ android-cache/src/main/java/me/juangoncalves/mentra/android_cache/mappers/CoinMapper.kt
@@ -6,7 +6,8 @@ import javax.inject.Inject
 
 class CoinMapper @Inject constructor() {
 
-    fun map(coin: Coin): CoinModel = CoinModel(coin.symbol, coin.imageUrl, coin.name, coin.iconType)
+    fun map(coin: Coin): CoinModel =
+        CoinModel(coin.symbol, coin.imageUrl, coin.name, coin.iconType, coin.position)
 
     fun map(model: CoinModel): Coin =
         Coin(model.name, model.symbol, model.imageUrl, model.iconType, model.position)
