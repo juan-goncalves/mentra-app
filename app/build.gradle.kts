760d5e5ebe22cea6d65c988560ee0a1eee11b5be
Juan Goncalves <juan.goncalves498@gmail.com>
2022-03-30 19:58:48 -0400

    Grunt: Update app version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "me.juangoncalves.mentra"
         minSdk = 26
         targetSdk = 31
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
